b231749573dbfe6012a44fbffb3c5683ea77c8c0
Eli Finer <eli.finer@gmail.com>
2014-12-26 14:41:11 +0200

    back to templates

--- fabfile.py
+++ fabfile.py
@@ -1,20 +1,40 @@
+import os
 from fabric.api import *
 from fabric.contrib.files import exists
 from fabric.contrib.project import rsync_project
 
 # application settings
-app_user = 'eli'
-app_name = 'testapp'
+config = type(env)()
+app_user = 'app_user'
+app_name = 'app_name'
 app_dir = '/home/%s/%s' % (app_user, app_name)
 
-source_supervisor_conf = '%s/etc/supervisor.conf' % app_dir
+local_supervisor_conf = './etc/supervisor.conf'
 system_supervisor_conf = '/etc/supervisor/conf.d/%s.conf' % app_name
-source_nginx_conf = '%s/etc/nginx.conf' % app_dir
+local_ngix_conf = './etc/nginx.conf'
 system_nginx_conf = '/etc/nginx/sites-enabled/%s' % app_name
 
 # fabric settings
 env.hosts = ['dev.elifiner.com']
 env.use_ssh_config = True
+
+nginx_conf_template = '''
+server {
+    location / {
+        proxy_pass http://127.0.0.1:8000;
+    }
+    location /static {
+        alias /home/%(app_user)s/%(app_name)s/static/;
+    }
+}
+''' % dict(app_user=app_user, app_name=app_name)
+
+supervisor_conf_template = '''
+[program:%(app_name)s]
+command = /home/%(app_user)s/%(app_name)s/venv/bin/gunicorn app:app -b localhost:8000
+directory = /home/%(app_user)s/%(app_name)s
+user = %(app_user)s
+''' % dict(app_user=app_user, app_name=app_name)
 
 def _install_requirements():
     with hide('stdout'):
@@ -26,12 +46,12 @@ def _install_requirements():
         sudo('apt-get install -y git')
 
 def _configure_supervisor():
-    sudo('ln -s %s %s' % (source_supervisor_conf, system_supervisor_conf))
+    sudo('ln -s %s/%s %s' % (app_path, local_ngix_conf, system_supervisor_conf))
     sudo('supervisorctl reread')
     sudo('supervisorctl update')
 
 def _configure_nginx():
-    sudo('ln -s %s %s' % (source_nginx_conf, system_nginx_conf))
+    sudo('ln -s %s/%s %s' % (app_path, source_nginx_conf, system_nginx_conf))
     sudo('/etc/init.d/nginx restart')
 
 def _sync_code():
@@ -45,6 +65,15 @@ def _update_venv():
             run('virtualenv venv --prompt="(%s)"' % app_name)
         with prefix('source venv/bin/activate'):
             run('pip install -r requirements.txt')
+
+def make_conf():
+    '''Generate nginx and supervisor configuration files'''
+    if not os.path.exists('etc'):
+        os.makedirs('etc')
+    with open(local_supervisor_conf, 'w') as f:
+        f.write(supervisor_conf_template)
+    with open(local_ngix_conf, 'w') as f:
+        f.write(nginx_conf_template)
 
 def install():
     _install_requirements()
